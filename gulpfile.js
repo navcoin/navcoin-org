9a03c8ed907b84a1f3521a35271c8155623ad495
Matt <mpau548@aucklanduni.ac.nz>
2018-05-16 11:03:03 +1200

    some styling tweaks to the buttons and header dropdown arrows

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('sass', function(){
-  return gulp.src('themes/nav-community-v1/static/css/*.scss')
+gulp.task('sass', function () {
+  return gulp.src('themes/nav-community-v1/static/css/nav.scss')
     .pipe(sass())
     .pipe(gulp.dest('themes/nav-community-v1/static/css'))
 });
