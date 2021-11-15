660d1dc805958a75589208a31dd1a413224275ab
anquii <50277099+anquii@users.noreply.github.com>
2021-11-15 16:16:18 +0000

    Formatting

--- static/js/bounties.js
+++ static/js/bounties.js
@@ -13,8 +13,8 @@ function populateGrid(id, contentTemplateId, url) {
             const content = contentTemplate.cloneNode(true);
             const topBlock = content.querySelector('.accordion-top-block');
             const bottomBlock = content.querySelector('.accordion-bottom-block');
-          	const paragraph = content.querySelector('.accordion-paragraph');
-			content.querySelector('.accordion-title').innerHTML = item.title;
+            const paragraph = content.querySelector('.accordion-paragraph');
+            content.querySelector('.accordion-title').innerHTML = item.title;
             content.querySelector('.bounty-programs-button').href = item.bountiesURL;
             content.id = item.id;
 
